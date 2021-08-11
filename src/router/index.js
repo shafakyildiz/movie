618c68a1a9fe665dd718600992c4553be3505faa
Şafak Yıldız <shafakyildiz@gmail.com>
2021-08-12 01:41:37 +0300

    comment deleted

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Detail from "../views/Detail";
-// import MovieDetail from "../views/MovieDetail.vue";
 
 const routes = [
   {
