579620d6ffff5f54fee00543b39dc07480f3f9ce
shafakyildiz <shafakyildiz@gmail.com>
2024-09-01 14:45:06 +0300

    fix: change styling

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,7 +5,7 @@
       <img :src="movie.poster" alt="Movie Poster" />
     </div>
     <h1>{{ movie.title }}</h1>
-    <h3>{{ movie.overview }}</h3>
+    <h3 class="movie-overview">{{ movie.overview }}</h3>
     <br />
   </div>
 </template>
@@ -31,5 +31,13 @@ h3,
   text-align: center;
   align-items: center;
   color: rgb(211, 206, 198);
+}
+
+.movie-overview {
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
 }
 </style>
